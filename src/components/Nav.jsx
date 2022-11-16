6de9cfbd700228901ee6b60e8e8a3c2e0c2b1a67
chiomavera <you@example.com>
2022-11-16 01:43:45 +0100

    MAKE: format document, add margin right to navlinks

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,35 +1,41 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import Logo from '../assets/logo.svg'
-import Button from './Button';
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Logo from "../assets/logo.svg";
+import Button from "./Button";
 
 const TopNavBar = () => {
-    return (
-      <Navbar bg="white" expand="lg">
-        <Container>
-          <Navbar.Brand>
+  return (
+    <Navbar bg="white" expand="lg">
+      <Container>
+        <Navbar.Brand>
           <img
-              alt="logo"
-              src={Logo}
-              width="150"
-              height="50"
-              className="d-inline-block align-top"
-            />
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="mx-auto">
-              <Nav.Link href="/">Home</Nav.Link>
-              <Nav.Link href="/place-to-stay">Place to stay</Nav.Link>
-              <Nav.Link href="#nfts">NFTs</Nav.Link>
-              <Nav.Link href="#community">Community</Nav.Link>
-            </Nav>
-            <Button />
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    );
-  }
-  
-  export default TopNavBar;
+            alt="Brand logo"
+            src={Logo}
+            width="150"
+            height="50"
+            className="d-inline-block align-top"
+          />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mx-auto">
+            <Nav.Link href="/" className="me-3">
+              Home
+            </Nav.Link>
+            <Nav.Link href="/place-to-stay" className="me-3">
+              Place to stay
+            </Nav.Link>
+            <Nav.Link href="#nfts" className="me-3">
+              NFTs
+            </Nav.Link>
+            <Nav.Link href="#community">Community</Nav.Link>
+          </Nav>
+          <Button />
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default TopNavBar;
