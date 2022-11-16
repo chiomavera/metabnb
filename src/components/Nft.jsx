13f496e5e42f5e2861cbbc99831a79b8b57a3492
chiomavera <you@example.com>
2022-11-16 01:38:46 +0100

    FEAT: create Nft component

---
+++ src/components/Nft.jsx
@@ -0,0 +1,37 @@
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import FooterImage from "../assets/footer-image.webp";
+const Nfts = () => {
+  return (
+    <main className="Nfts mt-5">
+      <Container>
+        <Row>
+          <Col lg={6}>
+            <h1 className="Nfts-heading mt-5">Metabnb NFTs</h1>
+            <p className="Nfts-text">
+              Discover our NFT gift cards collection. Loyal customers gets
+              amazing gift cards which are traded as NFTs. These NFTs gives our
+              customer access to loads of our exclusive services.
+            </p>
+            <button className="Nfts-btn">
+              <a href="#learn-more">Learn more</a>
+            </button>
+          </Col>
+          <Col lg={6}>
+            <div className="hero-image-box mb-5">
+              <img
+                src={FooterImage}
+                alt="Beautiful nfts"
+                width="300"
+                className="hero-image"
+              />
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </main>
+  );
+};
+
+export default Nfts;
