48b9a5362493c9e333b2b5d588ed2b0ae260804d
chiomavera <you@example.com>
2022-11-16 01:29:00 +0100

    MAKE: format document

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,8 @@
 import { Link } from "react-router-dom";
 import React, { useState } from "react";
 import Modal from "react-bootstrap/Modal";
-import MetaMask from "../assets/Frame 37.png"
-import WalletConnect from "../assets/Frame 38.png"
-
+import MetaMask from "../assets/Frame 37.png";
+import WalletConnect from "../assets/Frame 38.png";
 
 const Button = () => {
   const [show, setShow] = useState(false);
@@ -17,17 +16,30 @@ const Button = () => {
           <Link to="#connect-wallet">Connect Wallet</Link>
         </button>
       </div>
-      <Modal show={show} onHide={handleClose} animation={false} className="modal-box">
+      <Modal
+        show={show}
+        onHide={handleClose}
+        animation={false}
+        className="modal-box"
+      >
         <Modal.Header closeButton>
           <Modal.Title className="modal-title">Connect Wallet</Modal.Title>
         </Modal.Header>
         <Modal.Body>
           <p>Choose your preferred wallet:</p>
           <div className="mb-3 modal-button">
-          <a href="#metamask"><img src={MetaMask} alt="meta mask" className="modal-image"/></a>
+            <a href="#metamask">
+              <img src={MetaMask} alt="meta mask" className="modal-image" />
+            </a>
           </div>
           <div className="modal-button">
-          <a href="#wallet connect"><img src={WalletConnect} alt="wallet connect" className="modal-image"/></a>
+            <a href="#wallet connect">
+              <img
+                src={WalletConnect}
+                alt="wallet connect"
+                className="modal-image"
+              />
+            </a>
           </div>
         </Modal.Body>
       </Modal>
--- src/components/SecondNav.jsx
+++ src/components/SecondNav.jsx
@@ -1,41 +1,61 @@
-import Nav from 'react-bootstrap/Nav';
-import Container from 'react-bootstrap/Container';
-import LocationIcon from '../assets/setting-5.svg'
+import Nav from "react-bootstrap/Nav";
+import Container from "react-bootstrap/Container";
+import LocationIcon from "../assets/setting-5.svg";
 
 const SecondNav = () => {
   return (
-    <Container className='mt-5 mb-5' bg="white">
-        <Nav as="ul">
-      <Nav.Item className='nav-item' as="li">
-        <Nav.Link href="#" className='nav-link'>Resturant</Nav.Link>
-      </Nav.Item>
-      <Nav.Item className='nav-item' as="li">
-        <Nav.Link href="#" className='nav-link'>Cottage</Nav.Link>
-      </Nav.Item>
-      <Nav.Item className='nav-item' as="li">
-        <Nav.Link href="#" className='nav-link'>Castle</Nav.Link>
-      </Nav.Item>
-      <Nav.Item className='nav-item' as="li">
-        <Nav.Link href="#" className='nav-link'>Fantast city</Nav.Link>
-      </Nav.Item>
-      <Nav.Item className='nav-item' as="li">
-        <Nav.Link href="#" className='nav-link'>Beach</Nav.Link>
-      </Nav.Item>
-      <Nav.Item className='nav-item' as="li">
-        <Nav.Link href="#" className='nav-link'>Carbins</Nav.Link>
-      </Nav.Item>
-      <Nav.Item className='nav-item' as="li">
-        <Nav.Link href="#" className='nav-link'>Off-grid</Nav.Link>
-      </Nav.Item>
-      <Nav.Item className='nav-item' as="li">
-        <Nav.Link href="#" className='nav-link'>farm</Nav.Link>
-      </Nav.Item>
-      <Nav.Item as="li">
-        <button className='location'><Nav.Link href="#" className='nav-link location-link'>Location <img src={LocationIcon} alt=''/></Nav.Link></button>
-      </Nav.Item>
-    </Nav>
+    <Container className="mt-5 mb-5" bg="white">
+      <Nav as="ul">
+        <Nav.Item className="nav-item" as="li">
+          <Nav.Link href="#" className="nav-link">
+            Resturant
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item className="nav-item" as="li">
+          <Nav.Link href="#" className="nav-link">
+            Cottage
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item className="nav-item" as="li">
+          <Nav.Link href="#" className="nav-link">
+            Castle
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item className="nav-item" as="li">
+          <Nav.Link href="#" className="nav-link">
+            Fantast city
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item className="nav-item" as="li">
+          <Nav.Link href="#" className="nav-link">
+            Beach
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item className="nav-item" as="li">
+          <Nav.Link href="#" className="nav-link">
+            Carbins
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item className="nav-item" as="li">
+          <Nav.Link href="#" className="nav-link">
+            Off-grid
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item className="nav-item" as="li">
+          <Nav.Link href="#" className="nav-link">
+            farm
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item as="li">
+          <button className="location">
+            <Nav.Link href="#" className="nav-link location-link">
+              Location <img src={LocationIcon} alt="" />
+            </Nav.Link>
+          </button>
+        </Nav.Item>
+      </Nav>
     </Container>
   );
-}
+};
 
 export default SecondNav;
