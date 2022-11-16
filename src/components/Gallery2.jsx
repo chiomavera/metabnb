339046a958d37a53ec125692d68fe3dbc3d6a84c
chiomavera <you@example.com>
2022-11-16 01:40:44 +0100

    FEAT: create gallery2 component

---
+++ src/components/Gallery2.jsx
@@ -0,0 +1,150 @@
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import FirstImage from "../assets/Group 4048.png";
+import SecondImage from "../assets/Group 4049.png";
+import ThirdImage from "../assets/Group 4050.png";
+import FourthImage from "../assets/Group 4051.png";
+import FifthImage from "../assets/Group 4052.png";
+import SixthImage from "../assets/Group 4053.png";
+import SeventhImage from "../assets/Group 4054.png";
+import EightImage from "../assets/Group 4055.png";
+import NinthImage from "../assets/4056.png";
+import TenthImage from "../assets/4057.png";
+import EleventhImage from "../assets/4058.png";
+import TwelveImage from "../assets/4059.png";
+import ThirteenImage from "../assets/4060.png";
+import FourteenImage from "../assets/4061.png";
+import FifteenImage from "../assets/4062.png";
+import SixteenImage from "../assets/4063.png";
+
+const Gallery2 = () => {
+  return (
+    <Container className="mb-5">
+      <div className="gallery-house">
+        <Row>
+          <Col lg={3} md={6}>
+            <img
+              src={FifthImage}
+              alt="beautiful ntf"
+              className="gallery-image"
+            />
+          </Col>
+          <Col lg={3} md={6}>
+            <img
+              src={SixthImage}
+              alt="beautiful ntf"
+              className="gallery-image"
+            />
+          </Col>
+          <Col lg={3} md={6}>
+            <img
+              src={SeventhImage}
+              alt="beautiful ntf"
+              className="gallery-image"
+            />
+          </Col>
+          <Col lg={3} md={6}>
+            <img
+              src={EightImage}
+              alt="beautiful ntf"
+              className="gallery-image"
+            />
+          </Col>
+        </Row>
+        <Row>
+          <Col lg={3} md={6}>
+            <img
+              src={FirstImage}
+              alt="beautiful ntf"
+              className="gallery-image"
+            />
+          </Col>
+          <Col lg={3} md={6}>
+            <img
+              src={SecondImage}
+              alt="beautiful ntf"
+              className="gallery-image"
+            />
+          </Col>
+          <Col lg={3} md={6}>
+            <img
+              src={ThirdImage}
+              alt="beautiful ntf"
+              className="gallery-image"
+            />
+          </Col>
+          <Col lg={3} md={6}>
+            <img
+              src={FourthImage}
+              alt="beautiful ntf"
+              className="gallery-image"
+            />
+          </Col>
+        </Row>
+        <Row>
+          <Col lg={3} md={6}>
+            <img
+              src={NinthImage}
+              alt="beautiful ntf"
+              className="gallery-image"
+            />
+          </Col>
+          <Col lg={3} md={6}>
+            <img
+              src={TenthImage}
+              alt="beautiful ntf"
+              className="gallery-image"
+            />
+          </Col>
+          <Col lg={3} md={6}>
+            <img
+              src={EleventhImage}
+              alt="beautiful ntf"
+              className="gallery-image"
+            />
+          </Col>
+          <Col lg={3} md={6}>
+            <img
+              src={TwelveImage}
+              alt="beautiful ntf"
+              className="gallery-image"
+            />
+          </Col>
+        </Row>
+        <Row>
+          <Col lg={3} md={6}>
+            <img
+              src={ThirteenImage}
+              alt="beautiful ntf"
+              className="gallery-image"
+            />
+          </Col>
+          <Col lg={3} md={6}>
+            <img
+              src={FourteenImage}
+              alt="beautiful ntf"
+              className="gallery-image"
+            />
+          </Col>
+          <Col lg={3} md={6}>
+            <img
+              src={FifteenImage}
+              alt="beautiful ntf"
+              className="gallery-image"
+            />
+          </Col>
+          <Col lg={3} md={6}>
+            <img
+              src={SixteenImage}
+              alt="beautiful ntf"
+              className="gallery-image"
+            />
+          </Col>
+        </Row>
+      </div>
+    </Container>
+  );
+};
+
+export default Gallery2;
