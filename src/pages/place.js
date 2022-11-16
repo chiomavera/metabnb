8ef30c4a3a5077b12ab94fcd2aa95a7b948bb3da
chiomavera <you@example.com>
2022-11-16 01:38:02 +0100

    FEAT: create place to stay page

---
+++ src/pages/place.js
@@ -0,0 +1,17 @@
+import TopNavBar from "../components/Nav";
+import SecondNav from "../components/SecondNav";
+import Footer from "../components/Footer";
+import Gallery2 from "../components/Gallery2";
+
+const Place = () => {
+  return (
+    <div>
+      <TopNavBar />
+      <SecondNav />
+      <Gallery2 />
+      <Footer />
+    </div>
+  );
+};
+
+export default Place;
