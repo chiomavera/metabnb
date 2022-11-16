62d84515d93c5e54beef5c39b4ab0b7e9995dbb8
chiomavera <you@example.com>
2022-11-16 12:23:37 +0100

    MAKE: fix container padding and image width

--- src/components/Nft.jsx
+++ src/components/Nft.jsx
@@ -4,11 +4,11 @@ import Col from "react-bootstrap/Col";
 import FooterImage from "../assets/footer-image.webp";
 const Nfts = () => {
   return (
-    <main className="Nfts mt-5">
+    <main className="Nfts mt-5 pb-5">
       <Container>
         <Row>
-          <Col lg={6}>
-            <h1 className="Nfts-heading mt-5">Metabnb NFTs</h1>
+          <Col lg={6} className='pb-5'>
+            <h1 className="Nfts-heading">Metabnb NFTs</h1>
             <p className="Nfts-text">
               Discover our NFT gift cards collection. Loyal customers gets
               amazing gift cards which are traded as NFTs. These NFTs gives our
@@ -24,7 +24,7 @@ const Nfts = () => {
                 src={FooterImage}
                 alt="Beautiful nfts"
                 width="300"
-                className="hero-image"
+                className="Nfts-image"
               />
             </div>
           </Col>
