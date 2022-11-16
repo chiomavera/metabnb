a2af503df9d1c8da195c2eaf3079dfec6c416e81
chiomavera <you@example.com>
2022-11-16 01:35:46 +0100

    FEAT: add gallery1 NftSection and Footer components

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,12 +1,18 @@
 import TopNavBar from "../components/Nav";
 import Hero from "../components/Hero";
+import Gallery1 from "../components/Gallery1";
+import NftSection from "../components/Nft";
+import Footer from "../components/Footer";
 const Home = () => {
-    return (
-      <div>
-           <TopNavBar />
-           <Hero />
-      </div>
-    );
-  }
-  
-  export default Home;
+  return (
+    <div>
+      <TopNavBar />
+      <Hero />
+      <Gallery1 />
+      <NftSection />
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
